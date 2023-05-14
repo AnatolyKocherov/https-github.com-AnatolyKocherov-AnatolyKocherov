124ea08f9a2bb893f42b361b9356e33c4c1a5611
AnatolyKocherov <131432382+AnatolyKocherov@users.noreply.github.com>
2023-05-14 20:47:13 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+int size = 0;
+int n = 100;
+int result = 1;
+
+while (index < n)
+{
+    
+    index++;
+    Console.WriteLine(index*index*index);
+}
+if (num > size)
+{
+    num = result * 3;
+    size++;
+    Console.WriteLine(size);
+    
+}
+
+
+ else if (num >= n)
+{
+    Console.WriteLine("Таблица построенна");
+}
