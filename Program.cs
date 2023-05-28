fd6d8f5cc86cf41a30b0e1911ec1ada2b85c78fa
AnatolyKocherov <131432382+AnatolyKocherov@users.noreply.github.com>
2023-05-28 13:59:01 +0700

    Домашняя работа
    Функции

--- Program.cs
+++ Program.cs
@@ -1,28 +1,63 @@
-﻿
+﻿        //  Домашняя работа .Кочеров Анатолий.
 
-Console.WriteLine("Введите число");
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+// System.Console.WriteLine("Введите числа");
+// int i = Convert.ToInt32(Console.ReadLine());
+//     System.Console.WriteLine("Введите числа");
+// int i1 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.WriteLine("Введите числа");
+// int i2 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.WriteLine("Введите числа");
+// int i3 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.WriteLine("Введите числа");
+// int i4 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.WriteLine("Введите числа");
+// int i5 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.WriteLine("Введите числа");
+// int i6 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.WriteLine("Введите числа");
+// int i7 = Convert.ToInt32(Console.ReadLine()); 
+//     for(int num = 0; num < 1; num++)
+//     {
+//         System.Console.WriteLine($"Массив : [{i},{i1},{i2},{i3},{i4},{i5},{i6},{i7}]");
+//     }
+
+
+
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите первое число");
 int num = Convert.ToInt32(Console.ReadLine());
-int index = 0;
-int size = 0;
-int n = 100;
-int result = 1;
-
-while (index < n)
+System.Console.WriteLine("Введите второе число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int oppo = 0;
+int result = 0;
+int n = 0;
+while (oppo < 1)
 {
+    while (n < 1)
+
+
+    {
+         result = (num*num)*num1;
+        System.Console.WriteLine($"Результат возведения в степень : {   result  }");
+        n++;
+    }
+   
     
-    index++;
-    Console.WriteLine(index*index*index);
+    oppo++;
+   
 }
-if (num > size)
-{
-    num = result * 3;
-    size++;
-    Console.WriteLine(size);
-    
-}
-
-
- else if (num >= n)
-{
-    Console.WriteLine("Таблица построенна");
-}
+return result;
