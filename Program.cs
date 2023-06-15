f206646cf68f210f313e532e3f2c5ecc9dc7088b
AnatolyKocherov <131432382+AnatolyKocherov@users.noreply.github.com>
2023-06-15 10:24:28 +0700

    Анатолий Кочеров
    Рекурсия

--- Program.cs
+++ Program.cs
@@ -1,63 +1,35 @@
-﻿        //  Домашняя работа .Кочеров Анатолий.
-
-// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-
-// 6, 1, 33 -> [6, 1, 33]
-
-
-
-// System.Console.WriteLine("Введите числа");
-// int i = Convert.ToInt32(Console.ReadLine());
-//     System.Console.WriteLine("Введите числа");
-// int i1 = Convert.ToInt32(Console.ReadLine()); 
-// System.Console.WriteLine("Введите числа");
-// int i2 = Convert.ToInt32(Console.ReadLine()); 
-// System.Console.WriteLine("Введите числа");
-// int i3 = Convert.ToInt32(Console.ReadLine()); 
-// System.Console.WriteLine("Введите числа");
-// int i4 = Convert.ToInt32(Console.ReadLine()); 
-// System.Console.WriteLine("Введите числа");
-// int i5 = Convert.ToInt32(Console.ReadLine()); 
-// System.Console.WriteLine("Введите числа");
-// int i6 = Convert.ToInt32(Console.ReadLine()); 
-// System.Console.WriteLine("Введите числа");
-// int i7 = Convert.ToInt32(Console.ReadLine()); 
-//     for(int num = 0; num < 1; num++)
-//     {
-//         System.Console.WriteLine($"Массив : [{i},{i1},{i2},{i3},{i4},{i5},{i6},{i7}]");
-//     }
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
 
 
 
 
-// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-// 3, 5 -> 243 (3⁵)
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-// 2, 4 -> 16
-
-System.Console.WriteLine("Введите первое число");
-int num = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите второе число");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int oppo = 0;
-int result = 0;
-int n = 0;
-while (oppo < 1)
+class Program 
 {
-    while (n < 1)
 
 
-    {
-         result = (num*num)*num1;
-        System.Console.WriteLine($"Результат возведения в степень : {   result  }");
-        n++;
-    }
-   
-    
-    oppo++;
-   
+
+
+  static void n(int i)
+  {
+      System.Console.WriteLine(i);
+
+      if (i <= 1)
+        return;
+
+
+      i--;
+      n(i);
+  }
+
+
+  static void Main(string[] args)
+  {
+      n(8);
+  }
 }
-return result;
